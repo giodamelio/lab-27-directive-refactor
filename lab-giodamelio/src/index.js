a45ff08da759bb38bfddde28da302e2c3fb15d73
Gio d'Amelio <giodamelio@gmail.com>
2016-08-16 10:51:59 -0700

    Basic cowsay functionality

--- lab-giodamelio/src/index.js
+++ lab-giodamelio/src/index.js
@@ -1,10 +1,22 @@
 const angular = require('angular');
+const cowsay = require('cowsay-browser');
 
 require('!!file?name=[name].[ext]!./html/index.html');
 require('./scss/index.scss');
 
 const app = angular.module('cowsay', []);
 
-app.controller('say', ['$scope', function($scope) {
+app.controller('CowsayController', ['$scope', function($scope) {
+  $scope.say = function(text) {
+    return cowsay.say({
+      text,
+    });
+  }
+
+  $scope.save = function() {
+    $scope.result = $scope.say($scope.text);
+  }
+
   $scope.text = 'Hello World!';
+  $scope.result = $scope.say($scope.text);
 }]);
