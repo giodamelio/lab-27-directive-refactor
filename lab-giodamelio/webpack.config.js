9ddcf881f4d82f7e139de5f17e74c0294991a2f2
Gio d'Amelio <giodamelio@gmail.com>
2016-08-16 10:05:01 -0700

    Install webpack stuff from yesterday

---
+++ lab-giodamelio/webpack.config.js
@@ -0,0 +1,22 @@
+const path = require('path');
+
+const ExtractText = require('extract-text-webpack-plugin');
+
+module.exports = {
+  entry: path.join(__dirname, 'src/index.js'),
+  output: {
+    path: 'build/',
+    filename: 'bundle.js',
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.scss$/,
+        loader: ExtractText.extract('style', ['css', 'sass']),
+      },
+    ],
+  },
+  plugins: [
+    new ExtractText('bundle.css'),
+  ],
+};
