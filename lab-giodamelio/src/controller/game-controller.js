3698ae5dae072ad1073ad7365477562835d116cd
Gio d'Amelio <giodamelio@gmail.com>
2016-08-16 18:47:54 -0700

    Remove cowsay. Move controller into seperate file

---
+++ lab-giodamelio/src/controller/game-controller.js
@@ -0,0 +1,7 @@
+const angular = require('angular');
+
+const app = angular.module('game');
+
+app.controller('GameController', ['$scope', function($scope) {
+  $scope.gameCtl = {};
+}]);
--- lab-giodamelio/src/index.js
+++ lab-giodamelio/src/index.js
@@ -1,33 +1,8 @@
 const angular = require('angular');
-const cowsay = require('cowsay-browser');
 
 require('!!file?name=[name].[ext]!./html/index.html'); // eslint-disable-line
 require('./scss/index.scss');
 
-const app = angular.module('cowsay', []);
+const app = angular.module('game', []);
 
-app.controller('CowsayController', ['$scope', function($scope) {
-  $scope.state = {
-    history: [],
-  };
-
-  $scope.say = function(text) {
-    return cowsay.say({
-      text,
-    });
-  }
-
-  $scope.save = function() {
-    $scope.state.history.push($scope.result);
-    $scope.result = $scope.say($scope.text);
-  }
-
-  $scope.undo = function() {
-    var last = $scope.state.history.pop();
-    console.log(last, $scope.state.history);
-    $scope.result = last;
-  }
-
-  $scope.text = 'Hello World!';
-  $scope.result = $scope.say($scope.text);
-}]);
+require('./controller/game-controller');
