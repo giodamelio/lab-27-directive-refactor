e1dbe241a87abd500127e0b9252304076d280f8c
Gio d'Amelio <giodamelio@gmail.com>
2016-08-16 22:02:16 -0700

    Configure karma to run with webpack and babel

--- lab-giodamelio/karma.conf.js
+++ lab-giodamelio/karma.conf.js
@@ -1,5 +1,4 @@
-// Karma configuration
-// Generated on Tue Aug 16 2016 21:45:58 GMT-0700 (PDT)
+const webpackConfig = require('./webpack.config');
 
 module.exports = function(config) {
   config.set({
@@ -15,8 +14,9 @@ module.exports = function(config) {
 
     // list of files / patterns to load in the browser
     files: [
-      'src/**/*.js',
+      'src/index.js',
       'test/*-test.js',
+      'node_modules/angular-mocks/angular-mocks.js',
     ],
 
 
@@ -28,7 +28,11 @@ module.exports = function(config) {
     // preprocess matching files before serving them to the browser
     // available preprocessors:
     // https://npmjs.org/browse/keyword/karma-preprocessor
-    preprocessors: {},
+    webpack: webpackConfig,
+    preprocessors: {
+      'src/index.js': ['webpack'],
+      'test/*-test.js': ['babel'],
+    },
 
 
     // test results reporter to use
--- lab-giodamelio/src/index.js
+++ lab-giodamelio/src/index.js
@@ -3,6 +3,6 @@ const angular = require('angular');
 require('!!file?name=[name].[ext]!./html/index.html'); // eslint-disable-line
 require('./scss/index.scss');
 
-const app = angular.module('game', []);
+angular.module('game', []);
 
 require('./controller/game-controller');
