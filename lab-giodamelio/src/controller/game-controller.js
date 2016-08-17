1f09ea2ab5f705714eb3e82c65b1adbba9ff505d
Gio d'Amelio <giodamelio@gmail.com>
2016-08-16 19:52:17 -0700

    Basic player form

--- lab-giodamelio/src/controller/game-controller.js
+++ lab-giodamelio/src/controller/game-controller.js
@@ -3,5 +3,19 @@ const angular = require('angular');
 const app = angular.module('game');
 
 app.controller('GameController', ['$scope', function($scope) {
-  $scope.gameCtl = {};
+  $scope.gameCtl = {
+    genders: ['Male', 'Female', 'Other'],
+    races: [
+      'Dragonborn',
+      'Dwarf',
+      'Eladrin',
+      'Elf',
+      'Gnome',
+      'Half-elf',
+      'Half-orc',
+      'Halflin',
+      'Human',
+      'Tiefling',
+    ],
+  };
 }]);
