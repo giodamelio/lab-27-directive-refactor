be16106092ff1d087af7dab75e278c7f21cf3a6b
Gio d'Amelio <giodamelio@gmail.com>
2016-08-17 20:56:36 -0700

    Populate directions with ng-options

--- lab-giodamelio/src/controller/game-controller.js
+++ lab-giodamelio/src/controller/game-controller.js
@@ -3,4 +3,9 @@ const angular = require('angular');
 const app = angular.module('game');
 
 app.controller('GameController', ['$log', function($log) {
+  $log.log('GameController init');
+
+  this.directions = ['North', 'East', 'South', 'West'];
+
+  this.direction = 'North';
 }]);
