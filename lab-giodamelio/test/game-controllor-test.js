7716d1680d4e05ec957dcbcf52004cbbf633bc39
Gio d'Amelio <giodamelio@gmail.com>
2016-08-18 12:07:55 -0700

    Add test for addLogItem function

--- lab-giodamelio/test/game-controllor-test.js
+++ lab-giodamelio/test/game-controllor-test.js
@@ -10,4 +10,10 @@ describe('testing GameController', function() {
     this.gameCtrl.move('east');
     expect(this.gameCtrl.player.location).toBe('roomB');
   });
+
+  it('Add an item to the action log', () => {
+    expect(this.gameCtrl.actions.length).toBe(1);
+    this.gameCtrl.addLogItem('Test item');
+    expect(this.gameCtrl.actions.length).toBe(2);
+  });
 });
