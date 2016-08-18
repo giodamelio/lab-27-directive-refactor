751e92aaf44b953ca5398c9d5f431a23993da8bc
Gio d'Amelio <giodamelio@gmail.com>
2016-08-17 22:13:20 -0700

    Basic movement

--- lab-giodamelio/src/controller/game-controller.js
+++ lab-giodamelio/src/controller/game-controller.js
@@ -1,13 +1,43 @@
 const angular = require('angular');
 
+const map = require('../lib/map');
+
 const app = angular.module('game');
 
 app.controller('GameController', ['$log', function($log) {
   $log.log('GameController init');
 
-  this.directions = ['North', 'East', 'South', 'West'];
+  this.directions = ['north', 'east', 'south', 'west'];
+  this.map = map;
+  this.player = {
+    location: 'roomA',
+    hitpoints: 100,
+  };
 
-  this.direction = 'North';
+  this.direction = 'north';
+  this.actions = [
+    {
+      type: 'message',
+      text: 'As you enter the Dungeon, you hear the entry tunnel collapse behind you.',
+    },
+  ];
+
+  this.move = function() {
+    $log.log(`Attempting to move ${this.direction}`);
+    if (this.map[this.player.location]) {
+      const currentRoom = this.map[this.player.location];
+      const nextRoom = currentRoom[this.direction];
+
+      if (nextRoom === 'wall') {
+        return $log.log('You ran into a wall');
+      }
+
+      this.player.location = nextRoom;
+      return $log.log(`You enter ${nextRoom}`);
+    }
+
+    return $log.log(`Player location(${this.player.location}) is invalid`);
+  };
 }]);
 
 app.filter('capitalize', () => (input) => {
