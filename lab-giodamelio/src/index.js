6a3ceb6bc7adc5ceff897d1fabf16a896edc081f
Gio d'Amelio <giodamelio@gmail.com>
2016-08-22 11:08:30 -0700

    Remove old stuff

--- lab-giodamelio/src/index.js
+++ lab-giodamelio/src/index.js
@@ -2,7 +2,3 @@ const angular = require('angular');
 
 require('!!file?name=[name].[ext]!./html/index.html'); // eslint-disable-line
 require('./scss/index.scss');
-
-angular.module('game', []);
-
-require('./controller/game-controller');
