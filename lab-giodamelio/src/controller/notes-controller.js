1d631ad59c1899ebb8a1514b4bf14c70689d9e3a
Gio d'Amelio <giodamelio@gmail.com>
2016-08-22 20:29:34 -0700

    GET a list of people

--- lab-giodamelio/src/controller/notes-controller.js
+++ lab-giodamelio/src/controller/notes-controller.js
@@ -2,6 +2,18 @@ const angular = require('angular');
 
 const app = angular.module('notes');
 
-app.controller('NotesController', [function() {
-  this.test = 'Hello World';
+app.controller('PeopleController', ['$http', function($http) {
+  const peopleUrl = `${__API_URL__}/people`; // eslint-disable-line no-undef
+  this.people = [];
+
+  // Read the list of people from the server
+  this.readPeople = function() {
+    $http.get(peopleUrl)
+      .then((people) => {
+        this.people = people.data;
+      });
+  };
+
+  // Get the people once when the controller loads
+  this.readPeople();
 }]);
