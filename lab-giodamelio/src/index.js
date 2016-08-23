9b1f73b36a7de04bae00eae039b9859365020a8d
Gio d'Amelio <giodamelio@gmail.com>
2016-08-23 10:16:34 -0700

    Polyfill Array.prototype.find and Array.prototype.findIndex

--- lab-giodamelio/src/index.js
+++ lab-giodamelio/src/index.js
@@ -1,3 +1,5 @@
+require('core-js/fn/array/find');
+require('core-js/fn/array/find-index');
 const angular = require('angular');
 
 require('!!file?name=[name].[ext]!./html/index.html'); // eslint-disable-line
