0bcec6be8c2affc4fda931c7a0be572b80d2979e
Gio d'Amelio <giodamelio@gmail.com>
2016-08-23 19:36:05 -0700

    angular-route hello world

--- lab-giodamelio/src/index.js
+++ lab-giodamelio/src/index.js
@@ -1,4 +1,20 @@
+/* eslint global-require: "off" */
 const angular = require('angular');
+const angularRoute = require('angular-route');
 
 require('!!file?name=[name].[ext]!./html/index.html'); // eslint-disable-line
 require('./scss/index.scss');
+
+const app = angular.module('gallery', [angularRoute]);
+
+require('./view')(app);
+
+app.config(['$routeProvider', function($route) {
+  $route
+    .when('/', {
+      template: require('./view/home/home.html'),
+      controller: 'HomeController',
+      controllerAs: 'hc',
+    })
+    .otherwise({ redirectTo: '/error' });
+}]);
