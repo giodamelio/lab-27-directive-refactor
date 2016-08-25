14c38fb7bfa24b9a3bf78deba85c13f378809c93
Gio d'Amelio <giodamelio@gmail.com>
2016-08-24 22:31:10 -0700

    Add third directive

--- lab-giodamelio/src/component/index.js
+++ lab-giodamelio/src/component/index.js
@@ -2,4 +2,5 @@
 module.exports = function(app) {
   require('./animal-image-link/animal-image-link')(app);
   require('./animal-image-thumbnail/animal-image-thumbnail')(app);
+  require('./animal-image-full/animal-image-full')(app);
 };
