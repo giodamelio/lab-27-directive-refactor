a56b04d0fd070bf6a066b7955ad120f5d1a3afd4
Gio d'Amelio <giodamelio@gmail.com>
2016-09-05 14:32:43 -0700

    Add angular router

--- lab-giodamelio/src/index.js
+++ lab-giodamelio/src/index.js
@@ -1,7 +1,19 @@
 /* eslint global-require: "off" */
 const angular = require('angular');
+const angularRoute = require('angular-route');
 
 require('!!file?name=[name].[ext]!./html/index.html'); // eslint-disable-line
 require('./scss/index.scss');
 
-const app = angular.module('todoApp', []);
+const app = angular.module('todoApp', [angularRoute]);
+
+require('./view')(app);
+
+app.config(['$routeProvider', function($routeProvider) {
+  $routeProvider
+    .when('/', {
+      template: require('./view/home/home.html'),
+      controller: 'HomeController',
+      controllerAs: 'ctrl',
+    });
+}]);
---
+++ lab-giodamelio/src/view/home/home-controller.js
@@ -0,0 +1,5 @@
+module.exports = function(app) {
+  app.controller('HomeController', function() {
+    this.name = 'Gio';
+  });
+};
