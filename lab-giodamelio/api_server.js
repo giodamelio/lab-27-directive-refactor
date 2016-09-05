f2c41a38cba9b83656316c1d58a8518d2e230192
Gio d'Amelio <giodamelio@gmail.com>
2016-09-05 13:37:33 -0700

    Make the random ids shorter

--- lab-giodamelio/api_server.js
+++ lab-giodamelio/api_server.js
@@ -1,7 +1,11 @@
 const jsonServer = require('json-server');
 const Chance = require('chance');
+const shortid = require('shortid');
 
 const chance = new Chance();
+
+// Create a chance mixin for shortids
+chance.mixin({ shortid: () => shortid.generate() });
 
 // Create fake data
 const data = {};
@@ -10,7 +14,7 @@ const data = {};
 data.users = [];
 for (let i = 1; i <= 10; i++) {
   data.users.push({
-    id: chance.guid(),
+    id: chance.shortid(),
     name: chance.name(),
   });
 }
@@ -19,7 +23,7 @@ for (let i = 1; i <= 10; i++) {
 data.lists = [];
 for (let i = 1; i <= 100; i++) {
   data.lists.push({
-    id: chance.guid(),
+    id: chance.shortid(),
     userId: chance.pickone(data.users).id,
     title: chance.sentence({ words: 5 }).slice(0, -1),
   });
@@ -29,6 +33,7 @@ for (let i = 1; i <= 100; i++) {
 data.items = [];
 for (let i = 1; i <= 1000; i++) {
   data.items.push({
+    id: chance.shortid(),
     listId: chance.pickone(data.lists).id,
     text: chance.sentence({ words: 5 }).slice(0, -1),
     completed: chance.bool({ likelihood: 20 }),
