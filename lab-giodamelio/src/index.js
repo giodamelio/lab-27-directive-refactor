d43d3edeb6142ccbb665c32697ea630c145e4898
Gio d'Amelio <giodamelio@gmail.com>
2016-09-05 13:38:35 -0700

    Change the app from notes to todos

--- lab-giodamelio/src/index.js
+++ lab-giodamelio/src/index.js
@@ -4,4 +4,4 @@ const angular = require('angular');
 require('!!file?name=[name].[ext]!./html/index.html'); // eslint-disable-line
 require('./scss/index.scss');
 
-const app = angular.module('notesApp', []);
+const app = angular.module('todoApp', []);
