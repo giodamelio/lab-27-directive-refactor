2271ec19b5dc62b5766119fa30043bf26ddbc3c0
Gio d'Amelio <giodamelio@gmail.com>
2016-09-05 17:43:07 -0700

    Change list into table and show number of completed items

--- lab-giodamelio/src/services/lists.js
+++ lab-giodamelio/src/services/lists.js
@@ -2,7 +2,7 @@ module.exports = function(app) {
   app.factory('lists', ['$http', function($http) {
     return {
       getAll() {
-        return $http.get('/api/lists');
+        return $http.get('/api/lists?_embed=items');
       },
     };
   }]);
--- lab-giodamelio/src/view/home/home-controller.js
+++ lab-giodamelio/src/view/home/home-controller.js
@@ -2,9 +2,14 @@ module.exports = function(app) {
   app.controller('HomeController', ['lists', function(lists) {
     this.lists = [];
 
+    // Get all the lists
     lists.getAll()
       .then((response) => {
         this.lists = response.data;
       });
+
+    // Get the comleted items from a list
+    this.getCompletedItems = (list) =>
+	list.items.filter((item) => item.completed);
   }]);
 };
